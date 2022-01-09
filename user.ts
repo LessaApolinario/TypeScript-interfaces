fb601f1e8862ad864c1ff9301d47a19c85ed5fe7
Lessa Apolinario <lessaapolinario@gmail.com>
2022-01-09 13:05:53 -0300

    Add User interface

--- user.ts
+++ user.ts
@@ -1,17 +1,9 @@
 interface User {
   name: string;
-  id: number;
+  age: number;
   nickname?: string;
   email: string;
   password: string;
 }
 
-const user: User = {
-  name: 'Lessa Apolinario',
-  id: 225,
-  nickname: 'Neto',
-  email: 'email@example.com',
-  password: '123ew4'
-}
-
-export default user;
+export default User;
