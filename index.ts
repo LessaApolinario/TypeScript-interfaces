73b98bad7694d85b13fd0e8feefa1699e44d5a63
Lessa Apolinario <lessaapolinario@gmail.com>
2022-01-09 13:44:47 -0300

    Using intersections types

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import User from './user';
+import Person from './person';
 
 // Criando um objeto do tipo User
 // Notar que nickname é opcional
@@ -40,3 +41,22 @@ let adults: User[] = users.filter((user) => user.age >= 18);
 adults.map((user) =>
   console.log(`O usuário ${user.name} é adulto e tem ${user.age} anos`)
 );
+
+// Podemos ainda definir que um tipo recebe a intersecção de duas interfaces
+interface Colorful {
+  color: string;
+}
+
+// Podemos usar o | para dizer que uma variável recebe outros tipos
+interface Car {
+  speed: number | string;
+}
+
+type ColorfulCar = Colorful & Car;
+
+let myCar: ColorfulCar = {
+  color: 'Red',
+  speed: 120
+}
+
+console.log(`My ${myCar.color} car is at ${myCar.speed} kilometers an hour!`);
